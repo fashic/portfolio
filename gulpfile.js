87f2c9d9b38ef13ba5ae548aa1f049fc7793efd8
Alex Linker <fashic@bk/ru>
2018-09-27 13:23:44 +0500

    final html and css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace')
 
 //styles
 const sass = require('gulp-sass');
@@ -49,6 +53,43 @@ function templates() {
         }))
         .pipe(gulp.dest(paths.root));
 }
+
+const config = {
+    mode: {
+      symbol: {
+        sprite: "../sprite.svg",
+        example: {
+          dest: '../spriteSvgDemo.html' // демо html
+        }
+      }
+    }
+  };
+  
+  gulp.task('sprite', function() {
+    return gulp.src('./src/images/icons/*.svg')
+      // минифицируем svg
+      .pipe(svgmin({
+        js2svg: {
+          pretty: true
+        }
+      }))
+      // удалить все атрибуты fill, style and stroke в фигурах
+      .pipe(cheerio({
+        run: function($) {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+        },
+        parserOptions: {
+          xmlMode: true
+        }
+      }))
+      // cheerio плагин заменит, если появилась, скобка '&gt;', на нормальную.
+      .pipe(replace('&gt;', '>'))
+      // build svg sprite
+      .pipe(svgSprite(config))
+      .pipe(gulp.dest('./src/images/'));
+  });
 
 //scss 
 function styles() {
